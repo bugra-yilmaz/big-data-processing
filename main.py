cc35cbc6f9fdbcf4bdf9c5b424ecca55b66de217
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2021-06-13 12:24:36 +0200

    Initial approach to the problem

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import datetime
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
+from pyspark.sql.functions import date_add, datediff, col, sum, max, lit
 
 
 def parse_values(argument):
-    return [x.strip() for x in argument.split(',')]
+    return [x.strip() for x in argument.split(',') if x.strip()]
 
 
 if __name__ == '__main__':
@@ -43,3 +44,45 @@ if __name__ == '__main__':
 
     fact = spark.read.csv(fact_file_path, schema=schema_fact, header=True, dateFormat='dd/MM/yyyy HH:mm')
     lookup = spark.read.csv(lookup_file_path, schema=schema_lookup, header=True)
+
+    # Remove data with dates later than the reference date
+    fact = fact.filter(fact.EVENT_DATE < reference_date)
+
+    # Join dataframes
+    joined = fact.join(lookup, 'WEB_PAGEID').select(col('USER_ID'), col('EVENT_DATE'), col('WEBPAGE_TYPE'),
+                                                    lit(reference_date).alias('REFERENCE_DATE'))
+
+    # Remove data with other page types
+    joined = joined.filter(joined.WEBPAGE_TYPE.isin(page_types))
+
+    # Calculate end dates and time window matches for given time windows
+    result = None
+    fre_columns = []
+    for page_type in page_types:
+        aggs = []
+        df = joined.filter(joined.WEBPAGE_TYPE == page_type)
+
+        if 'dur' in metric_types:
+            aggs.append(datediff(max(df.REFERENCE_DATE), max(df.EVENT_DATE)).alias(f'pageview_{page_type}_dur'))
+
+        if 'fre' in metric_types:
+            for time_window in time_windows:
+                df = df. \
+                    withColumn(f'{time_window}_match',
+                               ((date_add(df.EVENT_DATE, time_window) > df.REFERENCE_DATE).cast('integer')))
+
+                fre_column = f'pageview_{page_type}_fre_{time_window}'
+                aggs.append(sum(col(f'{time_window}_match')).alias(fre_column))
+                fre_columns.append(fre_column)
+
+        df = df.groupby(['USER_ID']).agg(*aggs)
+
+        if result:
+            result = result.join(df, 'USER_ID', how='full')
+        else:
+            result = df
+
+    if 'fre' in metric_types:
+        result = result.fillna(0, fre_columns)
+
+    result.show(100)
