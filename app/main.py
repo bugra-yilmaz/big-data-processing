198c6b0ae51e702a635e46373d977cf8f9e62777
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2021-06-16 00:40:07 +0200

    Refactor code

--- app/main.py
+++ app/main.py
@@ -6,14 +6,18 @@
 import os
 import argparse
 import datetime
+from typing import List
 
 import pyspark.sql
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
 import pyspark.sql.functions as F
 
+StringList = List[str]
+IntList = List[int]
 
-def parse_argument(argument: str) -> list[str]:
+
+def parse_argument(argument: str) -> StringList:
     """
     Parses given command line argument by splitting it on commas and cleaning whitespaces.
 
@@ -25,7 +29,7 @@ def parse_argument(argument: str) -> list[str]:
 
     """
 
-    return [x.strip() for x in argument.split(',') if x.strip()]
+    return [x.strip() for x in argument.split(",") if x.strip()]
 
 
 def get_spark_session() -> pyspark.sql.SparkSession:
@@ -37,12 +41,20 @@ def get_spark_session() -> pyspark.sql.SparkSession:
 
     """
 
-    spark = SparkSession.builder.appName('adidas-bigdata-processing').master('local[*]').getOrCreate()
+    spark = (
+        SparkSession.builder.appName("adidas-bigdata-processing")
+        .master("local[*]")
+        .getOrCreate()
+    )
     return spark
 
 
-def filter_and_join_data(fact_df: pyspark.sql.DataFrame, lookup_df: pyspark.sql.DataFrame,
-                         reference_date: str, page_types: list[str]) -> pyspark.sql.DataFrame:
+def filter_by_date_and_join(
+    fact_df: pyspark.sql.DataFrame,
+    lookup_df: pyspark.sql.DataFrame,
+    reference_date: str,
+    page_types: StringList,
+) -> pyspark.sql.DataFrame:
     """
     Filters the given 'fact' dataframe by reference date and page types.
     After removing all events later than the reference date, joins the resulting dataframe with the 'lookup' dataframe.
@@ -60,21 +72,26 @@ def filter_and_join_data(fact_df: pyspark.sql.DataFrame, lookup_df: pyspark.sql.
 
     """
 
-    # Remove events with dates later than the reference date
+    # Remove events with dates later than the reference date as we don't need them in the analysis
     fact_df = fact_df.filter(fact_df.EVENT_DATE < F.lit(reference_date))
 
-    # Join dataframes - broadcast the lookup dataframe
-    fact_df = fact_df.join(F.broadcast(lookup_df), 'WEB_PAGEID').drop('WEB_PAGEID')
+    # Join dataframes, broadcast the lookup dataframe as it is a small, static file that does not need to be partitioned
+    fact_df = fact_df.join(F.broadcast(lookup_df), "WEB_PAGEID").drop("WEB_PAGEID")
 
-    # Remove data with other page types
+    # Remove data with other page types as we don't need them in the analysis
     fact_df = fact_df.filter(fact_df.WEBPAGE_TYPE.isin(page_types))
 
     return fact_df
 
 
-def get_intermediate_dataframe(fact_df: pyspark.sql.DataFrame, metric_types: list[str],
-                               page_type: str, reference_date: str, time_windows: list[int],
-                               frequency_column_names: list[str]) -> pyspark.sql.DataFrame:
+def get_intermediate_dataframe(
+    fact_df: pyspark.sql.DataFrame,
+    metric_types: StringList,
+    page_type: str,
+    reference_date: str,
+    time_windows: IntList,
+    frequency_column_names: StringList,
+) -> pyspark.sql.DataFrame:
     """
     Generates an intermediate dataframe from the given dataframe, containing all the metrics for a single page type.
     Resulting dataframes will later be joined to produce the final result.
@@ -95,38 +112,43 @@ def get_intermediate_dataframe(fact_df: pyspark.sql.DataFrame, metric_types: lis
 
     aggs = []
 
-    # Keep data with a single page type
+    # Keep data with a single page type to calculate metrics per page type
     temp_df = fact_df.filter(fact_df.WEBPAGE_TYPE == F.lit(page_type))
 
-    if 'dur' in metric_types:
-        recency_column_name = f'pageview_{page_type}_dur'
+    if "dur" in metric_types:
+        recency_column_name = f"pageview_{page_type}_dur"
 
         # Calculate recency metric as the difference between latest event date and the reference date
-        agg = F.datediff(F.lit(reference_date), F.max(temp_df.EVENT_DATE)). \
-            alias(recency_column_name)
+        agg = F.datediff(F.lit(reference_date), F.max(temp_df.EVENT_DATE)).alias(
+            recency_column_name
+        )
         aggs.append(agg)
 
-    if 'fre' in metric_types:
+    if "fre" in metric_types:
         for time_window in time_windows:
-            frequency_column_name = f'pageview_{page_type}_fre_{time_window}'
+            frequency_column_name = f"pageview_{page_type}_fre_{time_window}"
 
             # Calculate frequency metric as a count of event dates within the time window
-            agg = F.count(F.when(F.date_add(temp_df.EVENT_DATE, time_window) > F.lit(reference_date), 1)). \
-                alias(frequency_column_name)
+            agg = F.count(
+                F.when(
+                    F.date_add(temp_df.EVENT_DATE, time_window) > F.lit(reference_date),
+                    1,
+                )
+            ).alias(frequency_column_name)
             aggs.append(agg)
 
             # Keep frequency column names to replace null values later
             frequency_column_names.append(frequency_column_name)
 
-    # Apply all the aggregates on the intermediate dataframe
-    temp_df = temp_df.groupby(['USER_ID']).agg(*aggs)
+    # Apply all the aggregates on the intermediate dataframe to obtain requested metrics
+    temp_df = temp_df.groupby(["USER_ID"]).agg(*aggs)
 
     return temp_df
 
 
 def merge_csv_files(output_directory: str, output_file_path: str):
     """
-    A helper function that merges .csv files generated by worker nodes.
+    A helper function that merges .csv files generated by the worker nodes.
 
     Args:
         output_directory (str): Output directory that contains partitioned .csv files.
@@ -134,83 +156,135 @@ def merge_csv_files(output_directory: str, output_file_path: str):
 
     """
 
-    file_paths = [os.path.join(output_directory, f) for f in os.listdir(output_directory) if f.endswith('.csv')]
+    # Collect partitioned .csv files
+    file_paths = [
+        os.path.join(output_directory, f)
+        for f in os.listdir(output_directory)
+        if f.endswith(".csv")
+    ]
 
-    header = ''
-    content = ''
+    # Accumulate all the content in the .csv files
+    header = ""
+    content = ""
     for file_path in file_paths:
-        with open(file_path, 'r') as f:
+        with open(file_path, "r") as f:
             header = next(f)
             content += f.read()
     content = header + content
 
-    with open(output_file_path, 'w') as f:
+    with open(output_file_path, "w") as f:
         f.write(content)
 
 
-if __name__ == '__main__':
-    argument_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    argument_parser.add_argument('-p', '--page-type', help='Page types - separated by comma.', dest='p', metavar='')
-    argument_parser.add_argument('-m', '--metric-type', help='Metric types - separated by comma.', dest='m', metavar='')
-    argument_parser.add_argument('-t', '--time-window', help='Time windows - separated by comma.', dest='t', metavar='')
-    argument_parser.add_argument('-d', '--date-ref', help='Reference date.',
-                                 dest='d', metavar='')
-    argument_parser.add_argument('-np', '--n-partitions', help='Number of partitions for the main dataframe.',
-                                 dest='np', default=8, metavar='')
-    argument_parser.add_argument('-o', '--output', help='Output file path.', dest='o', default='output', metavar='')
+if __name__ == "__main__":
+    argument_parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    argument_parser.add_argument(
+        "-p",
+        "--page-type",
+        help="Page types - separated by comma.",
+        dest="p",
+        metavar="",
+    )
+    argument_parser.add_argument(
+        "-m",
+        "--metric-type",
+        help="Metric types - separated by comma.",
+        dest="m",
+        metavar="",
+    )
+    argument_parser.add_argument(
+        "-t",
+        "--time-window",
+        help="Time windows - separated by comma.",
+        dest="t",
+        metavar="",
+    )
+    argument_parser.add_argument(
+        "-d", "--date-ref", help="Reference date.", dest="d", metavar=""
+    )
+    argument_parser.add_argument(
+        "-np",
+        "--n-partitions",
+        help="Number of partitions for the main dataframe.",
+        dest="np",
+        default=8,
+        metavar="",
+    )
+    argument_parser.add_argument(
+        "-o",
+        "--output",
+        help="Output file path.",
+        dest="o",
+        default="output",
+        metavar="",
+    )
     args = argument_parser.parse_args()
 
     # Define input file paths
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    fact_file_path = os.path.join(dir_path, 'data', 'fact.csv')
-    lookup_file_path = os.path.join(dir_path, 'data', 'lookup.csv')
+    fact_file_path = os.path.join(dir_path, "data", "fact.csv")
+    lookup_file_path = os.path.join(dir_path, "data", "lookup.csv")
 
     # Parse command line arguments
     page_types = parse_argument(args.p)
     metric_types = parse_argument(args.m)
     time_windows = list(map(int, parse_argument(args.t)))
-    reference_date = datetime.datetime.strptime(args.d, '%d/%m/%Y').strftime('%Y-%m-%d')
+    reference_date = datetime.datetime.strptime(args.d, "%d/%m/%Y").strftime("%Y-%m-%d")
 
-    spark = SparkSession.builder.appName('adidas-bigdata-processing').master('local[*]').getOrCreate()
+    spark = (
+        SparkSession.builder.appName("adidas-bigdata-processing")
+        .master("local[*]")
+        .getOrCreate()
+    )
 
-    fact_schema = StructType([
-        StructField('USER_ID', IntegerType(), False),
-        StructField('EVENT_DATE', DateType(), False),
-        StructField('WEB_PAGEID', IntegerType(), False)
-    ])
+    fact_schema = StructType(
+        [
+            StructField("USER_ID", IntegerType(), False),
+            StructField("EVENT_DATE", DateType(), False),
+            StructField("WEB_PAGEID", IntegerType(), False),
+        ]
+    )
 
-    lookup_schema = StructType([
-        StructField('WEB_PAGEID', IntegerType(), False),
-        StructField('WEBPAGE_TYPE', StringType(), False),
-    ])
+    lookup_schema = StructType(
+        [
+            StructField("WEB_PAGEID", IntegerType(), False),
+            StructField("WEBPAGE_TYPE", StringType(), False),
+        ]
+    )
 
-    fact_df = spark.read.csv(fact_file_path, schema=fact_schema, header=True, dateFormat='dd/MM/yyyy HH:mm')
-
-    # Partition the dataframe by user id
-    fact_df = fact_df.repartition(args.np, 'USER_ID')
+    fact_df = spark.read.csv(
+        fact_file_path, schema=fact_schema, header=True, dateFormat="dd/MM/yyyy HH:mm"
+    )
+    fact_df = fact_df.repartition(args.np, "USER_ID")
 
     lookup_df = spark.read.csv(lookup_file_path, schema=lookup_schema, header=True)
 
-    fact_df = filter_and_join_data(fact_df, lookup_df, reference_date, page_types)
+    fact_df = filter_by_date_and_join(fact_df, lookup_df, reference_date, page_types)
 
     result_df = None
     frequency_column_names = []
     for page_type in page_types:
         # Calculate intermediate dataframe - metrics for a single page type
-        temp_df = get_intermediate_dataframe(fact_df, metric_types, page_type, reference_date,
-                                             time_windows, frequency_column_names)
+        temp_df = get_intermediate_dataframe(
+            fact_df,
+            metric_types,
+            page_type,
+            reference_date,
+            time_windows,
+            frequency_column_names,
+        )
 
-        # Join temporary (single page type) dataframe to the resulting dataframe by user id
         if result_df:
             # Full outer join to calculate null frequency and recency values
-            result_df = result_df.join(temp_df, 'USER_ID', how='full')
+            result_df = result_df.join(temp_df, "USER_ID", how="full")
         # If not initialized, start with the first intermediate dataframe
         else:
             result_df = temp_df
 
-    # Replace null frequency values with 0
-    if 'fre' in metric_types:
+    if "fre" in metric_types:
         result_df = result_df.fillna(0, frequency_column_names)
 
-    result_df.write.option('header', 'true').mode('overwrite').csv(args.o)
-    merge_csv_files(args.o, args.o + '.csv')
+    result_df.write.option("header", "true").mode("overwrite").csv(args.o)
+    merge_csv_files(args.o, args.o + ".csv")
--- app/tests.py
+++ app/tests.py
@@ -9,126 +9,221 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 import pyspark.sql.functions as F
 
-from main import parse_argument, get_spark_session, filter_and_join_data, get_intermediate_dataframe
+from main import (
+    parse_argument,
+    get_spark_session,
+    filter_by_date_and_join,
+    get_intermediate_dataframe,
+)
 
 
 # Provide Spark session fixture for unit tests
 @pytest.fixture
 def spark_session() -> pyspark.sql.SparkSession:
-    spark = SparkSession.builder.appName('adidas-bigdata-processing').master('local[*]').getOrCreate()
+    spark = (
+        SparkSession.builder.appName("adidas-bigdata-processing")
+        .master("local[*]")
+        .getOrCreate()
+    )
     return spark
 
 
-# Test argument parsing for command line arguments
 def test_parse_values():
-    argument, parsed = 'news, movies', ['news', 'movies']
+    argument, parsed = "news, movies", ["news", "movies"]
     assert parsed == parse_argument(argument)
 
-    argument, parsed = 'fre,dur  ', ['fre', 'dur']
+    argument, parsed = "fre,dur  ", ["fre", "dur"]
     assert parsed == parse_argument(argument)
 
-    argument, parsed = 'news,', ['news']
+    argument, parsed = "news,", ["news"]
     assert parsed == parse_argument(argument)
 
-    argument, parsed = '365,730, 1460,2920', ['365', '730', '1460', '2920']
+    argument, parsed = "365,730, 1460,2920", ["365", "730", "1460", "2920"]
     assert parsed == parse_argument(argument)
 
 
-# Test spark session generation
 def test_get_spark_session():
     spark_session = get_spark_session()
     assert type(spark_session) == SparkSession
 
-    data = [(710, '23/01/2017 15:43', 157), (446, '12/04/2016 09:09', 138),
-            (538, '28/04/2016 00:24', 196), (368, '27/04/2016 22:24', 196),
-            (9, '12/01/2018 13:48', 111)]
-    columns = ['USER_ID', 'EVENT_DATE', 'WEB_PAGEID']
+    data = [
+        (710, "23/01/2017 15:43", 157),
+        (446, "12/04/2016 09:09", 138),
+        (538, "28/04/2016 00:24", 196),
+        (368, "27/04/2016 22:24", 196),
+        (9, "12/01/2018 13:48", 111),
+    ]
+    columns = ["USER_ID", "EVENT_DATE", "WEB_PAGEID"]
 
     df = spark_session.createDataFrame(data, schema=columns)
     assert type(df) == DataFrame
 
 
-# Test initial filtering with reference date and page types, and join with lookup dataframe
-def test_filter_and_join_data(spark_session: pyspark.sql.SparkSession):
-    fact = [(710, '2016-08-18 09:42', 102), (710, '2016-02-29 14:24', 155), (619, '2016-03-01 15:27', 102),
-            (619, '2016-03-25 00:51', 188), (180, '2016-04-28 00:24', 196), (623, '2016-01-31 21:45', 126),
-            (710, '2016-04-27 22:24', 196), (87, '2018-11-26 22:04', 112), (479, '2016-04-27 22:24', 196),
-            (320, '2017-01-17 01:20', 149), (540, '2016-04-28 00:24', 196), (644, '2016-02-20 13:12', 190),
-            (970, '2016-04-28 00:24', 196), (970, '2016-06-30 14:02', 102), (701, '2016-01-17 11:40', 200),
-            (9, '2018-01-12 13:48', 111), (507, '2016-07-21 16:51', 157), (701, '2016-02-13 07:32', 102),
-            (651, '2016-04-28 00:24', 196), (160, '2016-04-28 00:24', 196), (710, '2016-04-27 22:24', 196),
-            (189, '2016-02-28 15:58', 102), (658, '2016-08-18 20:53', 166), (420, '2016-04-27 22:24', 196),
-            (710, '2016-04-27 22:24', 196), (47, '2018-11-26 22:04', 112), (285, '2016-02-03 08:49', 187)]
+def test_filter_by_date_and_join(spark_session: pyspark.sql.SparkSession):
+    fact = [
+        (710, "2016-08-18 09:42", 102),
+        (710, "2016-02-29 14:24", 155),
+        (619, "2016-03-01 15:27", 102),
+        (619, "2016-03-25 00:51", 188),
+        (180, "2016-04-28 00:24", 196),
+        (623, "2016-01-31 21:45", 126),
+        (710, "2016-04-27 22:24", 196),
+        (87, "2018-11-26 22:04", 112),
+        (479, "2016-04-27 22:24", 196),
+        (320, "2017-01-17 01:20", 149),
+        (540, "2016-04-28 00:24", 196),
+        (644, "2016-02-20 13:12", 190),
+        (970, "2016-04-28 00:24", 196),
+        (970, "2016-06-30 14:02", 102),
+        (701, "2016-01-17 11:40", 200),
+        (9, "2018-01-12 13:48", 111),
+        (507, "2016-07-21 16:51", 157),
+        (701, "2016-02-13 07:32", 102),
+        (651, "2016-04-28 00:24", 196),
+        (160, "2016-04-28 00:24", 196),
+        (710, "2016-04-27 22:24", 196),
+        (189, "2016-02-28 15:58", 102),
+        (658, "2016-08-18 20:53", 166),
+        (420, "2016-04-27 22:24", 196),
+        (710, "2016-04-27 22:24", 196),
+        (47, "2018-11-26 22:04", 112),
+        (285, "2016-02-03 08:49", 187),
+    ]
 
-    lookup = [(102, 'news'), (155, 'news'), (188, 'movies'), (196, 'news'), (126, 'movies'), (112, 'news'),
-              (149, 'news'), (190, 'news'), (200, 'news'), (111, 'news'), (157, 'news'), (166, 'news'),
-              (187, 'movies')]
-    fact_schema = StructType([
-        StructField('USER_ID', IntegerType(), False),
-        StructField('EVENT_DATE', StringType(), False),
-        StructField('WEB_PAGEID', IntegerType(), False)
-    ])
-    lookup_schema = StructType([
-        StructField('WEB_PAGEID', IntegerType(), False),
-        StructField('WEBPAGE_TYPE', StringType(), False),
-    ])
+    lookup = [
+        (102, "news"),
+        (155, "news"),
+        (188, "movies"),
+        (196, "news"),
+        (126, "movies"),
+        (112, "news"),
+        (149, "news"),
+        (190, "news"),
+        (200, "news"),
+        (111, "news"),
+        (157, "news"),
+        (166, "news"),
+        (187, "movies"),
+    ]
+    fact_schema = StructType(
+        [
+            StructField("USER_ID", IntegerType(), False),
+            StructField("EVENT_DATE", StringType(), False),
+            StructField("WEB_PAGEID", IntegerType(), False),
+        ]
+    )
+    lookup_schema = StructType(
+        [
+            StructField("WEB_PAGEID", IntegerType(), False),
+            StructField("WEBPAGE_TYPE", StringType(), False),
+        ]
+    )
 
     fact_df = spark_session.createDataFrame(fact, schema=fact_schema)
-    fact_df = fact_df.withColumn('EVENT_DATE', F.to_timestamp('EVENT_DATE'))
+    fact_df = fact_df.withColumn("EVENT_DATE", F.to_timestamp("EVENT_DATE"))
     lookup_df = spark_session.createDataFrame(lookup, schema=lookup_schema)
 
-    reference_date, page_types = '2019-10-12', ['sports']
-    joined_filtered_df = filter_and_join_data(fact_df, lookup_df, reference_date, page_types)
+    reference_date, page_types = "2019-10-12", ["sports"]
+    joined_filtered_df = filter_by_date_and_join(
+        fact_df, lookup_df, reference_date, page_types
+    )
     assert joined_filtered_df.count() == 0
 
-    reference_date, page_types = '2017-01-01', ['news']
-    joined_filtered_df = filter_and_join_data(fact_df, lookup_df, reference_date, page_types)
+    reference_date, page_types = "2017-01-01", ["news"]
+    joined_filtered_df = filter_by_date_and_join(
+        fact_df, lookup_df, reference_date, page_types
+    )
     assert joined_filtered_df.count() == 20
 
 
-# Test main computation stage where temporary dataframes are generated per page type with all metrics and time windows
 def test_get_intermediate_dataframe(spark_session: pyspark.sql.SparkSession):
-    fact = [(710, '2016-08-18 09:42', 102), (710, '2016-02-29 14:24', 155), (619, '2016-03-01 15:27', 102),
-            (619, '2016-03-25 00:51', 188), (180, '2016-04-28 00:24', 196), (623, '2016-01-31 21:45', 126),
-            (710, '2016-04-27 22:24', 196), (87, '2018-11-26 22:04', 112), (479, '2016-04-27 22:24', 196),
-            (320, '2017-01-17 01:20', 149), (540, '2016-04-28 00:24', 196), (644, '2016-02-20 13:12', 190),
-            (970, '2016-04-28 00:24', 196), (970, '2016-06-30 14:02', 102), (701, '2016-01-17 11:40', 200),
-            (9, '2018-01-12 13:48', 111), (507, '2016-07-21 16:51', 157), (701, '2016-02-13 07:32', 102),
-            (651, '2016-04-28 00:24', 196), (160, '2016-04-28 00:24', 196), (710, '2016-04-27 22:24', 196),
-            (189, '2016-02-28 15:58', 102), (658, '2016-08-18 20:53', 166), (420, '2016-04-27 22:24', 196),
-            (710, '2016-04-27 22:24', 196), (47, '2018-11-26 22:04', 112), (285, '2016-02-03 08:49', 187)]
+    fact = [
+        (710, "2016-08-18 09:42", 102),
+        (710, "2016-02-29 14:24", 155),
+        (619, "2016-03-01 15:27", 102),
+        (619, "2016-03-25 00:51", 188),
+        (180, "2016-04-28 00:24", 196),
+        (623, "2016-01-31 21:45", 126),
+        (710, "2016-04-27 22:24", 196),
+        (87, "2018-11-26 22:04", 112),
+        (479, "2016-04-27 22:24", 196),
+        (320, "2017-01-17 01:20", 149),
+        (540, "2016-04-28 00:24", 196),
+        (644, "2016-02-20 13:12", 190),
+        (970, "2016-04-28 00:24", 196),
+        (970, "2016-06-30 14:02", 102),
+        (701, "2016-01-17 11:40", 200),
+        (9, "2018-01-12 13:48", 111),
+        (507, "2016-07-21 16:51", 157),
+        (701, "2016-02-13 07:32", 102),
+        (651, "2016-04-28 00:24", 196),
+        (160, "2016-04-28 00:24", 196),
+        (710, "2016-04-27 22:24", 196),
+        (189, "2016-02-28 15:58", 102),
+        (658, "2016-08-18 20:53", 166),
+        (420, "2016-04-27 22:24", 196),
+        (710, "2016-04-27 22:24", 196),
+        (47, "2018-11-26 22:04", 112),
+        (285, "2016-02-03 08:49", 187),
+    ]
 
-    lookup = [(102, 'news'), (155, 'news'), (188, 'movies'), (196, 'news'), (126, 'movies'), (112, 'news'),
-              (149, 'news'), (190, 'news'), (200, 'news'), (111, 'news'), (157, 'news'), (166, 'news'),
-              (187, 'movies')]
-    fact_schema = StructType([
-        StructField('USER_ID', IntegerType(), False),
-        StructField('EVENT_DATE', StringType(), False),
-        StructField('WEB_PAGEID', IntegerType(), False)
-    ])
-    lookup_schema = StructType([
-        StructField('WEB_PAGEID', IntegerType(), False),
-        StructField('WEBPAGE_TYPE', StringType(), False),
-    ])
+    lookup = [
+        (102, "news"),
+        (155, "news"),
+        (188, "movies"),
+        (196, "news"),
+        (126, "movies"),
+        (112, "news"),
+        (149, "news"),
+        (190, "news"),
+        (200, "news"),
+        (111, "news"),
+        (157, "news"),
+        (166, "news"),
+        (187, "movies"),
+    ]
+    fact_schema = StructType(
+        [
+            StructField("USER_ID", IntegerType(), False),
+            StructField("EVENT_DATE", StringType(), False),
+            StructField("WEB_PAGEID", IntegerType(), False),
+        ]
+    )
+    lookup_schema = StructType(
+        [
+            StructField("WEB_PAGEID", IntegerType(), False),
+            StructField("WEBPAGE_TYPE", StringType(), False),
+        ]
+    )
     fact_df = spark_session.createDataFrame(fact, schema=fact_schema)
-    fact_df = fact_df.withColumn('EVENT_DATE', F.to_timestamp('EVENT_DATE'))
+    fact_df = fact_df.withColumn("EVENT_DATE", F.to_timestamp("EVENT_DATE"))
     lookup_df = spark_session.createDataFrame(lookup, schema=lookup_schema)
 
-    reference_date, page_types = '2017-10-12', ['news', 'movies']
-    fact_df = filter_and_join_data(fact_df, lookup_df, reference_date, page_types)
+    reference_date, page_types = "2017-10-12", ["news", "movies"]
+    fact_df = filter_by_date_and_join(fact_df, lookup_df, reference_date, page_types)
 
-    metric_types, page_type, time_windows = ['fre'], 'news', [365, 730]
-    temp_df = get_intermediate_dataframe(fact_df, metric_types, page_type, reference_date, time_windows, [])
-    assert temp_df.select(F.sum('pageview_news_fre_365')).collect()[0][0] == 1
-    assert temp_df.select(F.sum('pageview_news_fre_730')).collect()[0][0] == 21
+    metric_types, page_type, time_windows = ["fre"], "news", [365, 730]
+    temp_df = get_intermediate_dataframe(
+        fact_df, metric_types, page_type, reference_date, time_windows, []
+    )
+    assert temp_df.select(F.sum("pageview_news_fre_365")).collect()[0][0] == 1
+    assert temp_df.select(F.sum("pageview_news_fre_730")).collect()[0][0] == 21
 
-    metric_types, page_type, time_windows = ['fre', 'dur'], 'movies', [365, 730]
-    temp_df = get_intermediate_dataframe(fact_df, metric_types, page_type, reference_date, time_windows, [])
-    assert [val.pageview_movies_dur for val in temp_df.select('pageview_movies_dur').collect()] == [620, 617, 566]
-    assert temp_df.select(F.sum('pageview_movies_fre_730')).collect()[0][0] == 3
+    metric_types, page_type, time_windows = ["fre", "dur"], "movies", [365, 730]
+    temp_df = get_intermediate_dataframe(
+        fact_df, metric_types, page_type, reference_date, time_windows, []
+    )
+    assert [
+        val.pageview_movies_dur
+        for val in temp_df.select("pageview_movies_dur").collect()
+    ] == [620, 617, 566]
+    assert temp_df.select(F.sum("pageview_movies_fre_730")).collect()[0][0] == 3
 
-    metric_types, page_type, time_windows = ['dur'], 'news', [365, 730]
-    temp_df = get_intermediate_dataframe(fact_df, metric_types, page_type, reference_date, time_windows, [])
-    assert temp_df.schema.names == ['USER_ID', 'pageview_news_dur']
-    assert [val.pageview_news_dur for val in temp_df.select('pageview_news_dur').collect()] == \
-           [532, 469, 533, 448, 600, 592, 590, 420, 268, 532, 532, 420, 533, 532, 607]
+    metric_types, page_type, time_windows = ["dur"], "news", [365, 730]
+    temp_df = get_intermediate_dataframe(
+        fact_df, metric_types, page_type, reference_date, time_windows, []
+    )
+    assert temp_df.schema.names == ["USER_ID", "pageview_news_dur"]
+    assert [
+        val.pageview_news_dur for val in temp_df.select("pageview_news_dur").collect()
+    ] == [532, 469, 533, 448, 600, 592, 590, 420, 268, 532, 532, 420, 533, 532, 607]
